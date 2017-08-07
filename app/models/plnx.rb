c6efd410201949f6b5027956ca15231c299dc614
batmanov <batman@mail.ru>
2017-08-07 13:09:50 +0300

    ref threads

--- app/models/plnx.rb
+++ app/models/plnx.rb
@@ -4,8 +4,9 @@ require 'watir'
 require 'nokogiri'
 require 'benchmark'
 class Plnx
-  API_URL = 'https://poloniex.com/public?command=returnTicker'.freeze
-  WEB_URL = 'https://poloniex.com/exchange#'.freeze
+
+  API_URL = 'https://poloniex.com/public?command=returnTicker'
+  WEB_URL = 'https://poloniex.com/exchange#'
 
   def initialize
     @data = load_data
@@ -14,39 +15,33 @@ class Plnx
   def load_data
     response = HTTParty.get(API_URL)
     data = JSON.parse(response.body)
-    data.delete_if { |key, _value| Exchange.ex_types.keys.exclude? key }
+    data.delete_if {|key, value| Exchange.ex_types.keys.exclude? key}
   end
 
   def create_exchanges
-    threads = []
-    @data.each_slice(30) do |e|
-      new_h = Hash[e]
-      threads << Thread.new do
-        new_h.each do |key, value|
-          web_url = WEB_URL + key
-          browser = Watir::Browser.new(:phantomjs)
-          browser.goto(web_url)
-          sleep 1
-          document = Nokogiri::HTML(browser.html)
-          while document.css('#asksTotal').text.empty?
-            sleep 1
-            p "add 1 sec to parse' pair #{key}"
-            document = Nokogiri::HTML(browser.html)
-          end
-          browser.close
-          Exchange.create(ex_type: key,
-                          name: key.gsub('BTC_', ''),
-                          last_price: value['last'],
-                          percent_change: ((value['percentChange']).to_f * 100).round(2),
-                          high24hr: value['high24hr'],
-                          low24hr: value['low24hr'],
-                          sell_orders: document.css('#asksTotal').text,
-                          buy_orders: document.css('#bidsTotal').text,
-                          count_in_btc: 1.00 / (value['last']).to_f)
-        end
+    @data.each do |key, value|
+      web_url = WEB_URL + key
+      browser = Watir::Browser.new(:phantomjs)
+      browser.goto(web_url)
+      sleep 1
+      document = Nokogiri::HTML(browser.html)
+      while document.css('#asksTotal').text.empty?
+        sleep 1
+        p "add 1 sec to parse' pair #{key}"
+        document = Nokogiri::HTML(browser.html)
       end
+      browser.close
+      Exchange.create(ex_type: key,
+                      name: key.gsub('BTC_', ''),
+                      last_price: value['last'],
+                      percent_change: (((value['percentChange']).to_f)*100).round(2),
+                      high24hr: value['high24hr'],
+                      low24hr: value['low24hr'],
+                      sell_orders: document.css('#asksTotal').text,
+                      buy_orders: document.css('#bidsTotal').text,
+                      count_in_btc: 1.00/((value['last']).to_f))
+
     end
-    threads.each(&:join)
   end
 
   def self.start
@@ -55,4 +50,5 @@ class Plnx
     end
     $time_load = time.real
   end
+
 end
